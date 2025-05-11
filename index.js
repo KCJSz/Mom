2769f8b6b9359c5483895167d51cc8e4b96b8c6f
KCJSz <163737384+KCJSz@users.noreply.github.com>
2025-05-11 21:28:26 +0800

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function checkPassword() {
 
     if (passwordInput === correctPassword) {
         // Redirect to YouTube video
-        window.location.href = "https://youtube.com/shorts/Ew7xb_9SFyQ?feature=shared"; // Replace with your YouTube URL
+        window.location.href = "https://youtube.com/shorts/sF2ESzLtGeQ?feature=shared"; // Replace with your YouTube URL
     } else {
         message.style.color = "red";
         message.textContent = "Incorrect password. Try again!";
